0fc8afa8476a187fcc93ae0052d1317aad191af3
developer <developer@test.com>
2018-09-29 18:11:06 -0700

    Initial Project Add

---
+++ src/main/java/jt/springframework/jtpetclinic/JtPetClinicApplication.java
@@ -0,0 +1,12 @@
+package jt.springframework.jtpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtPetClinicApplication.class, args);
+	}
+}
---
+++ src/test/java/jt/springframework/jtpetclinic/JtPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package jt.springframework.jtpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JtPetClinicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
