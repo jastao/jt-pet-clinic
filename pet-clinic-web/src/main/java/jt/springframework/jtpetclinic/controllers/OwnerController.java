a8c84cf54f5bcd6881aae60ec656ca1a6fee581a
developer <developer@test.com>
2018-10-01 23:01:08 -0700

    List all the owners in the owner page. Closes #18

--- pet-clinic-web/src/main/java/jt/springframework/jtpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/jt/springframework/jtpetclinic/controllers/OwnerController.java
@@ -1,6 +1,8 @@
 package jt.springframework.jtpetclinic.controllers;
 
+import jt.springframework.jtpetclinic.services.map.OwnerMapService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -11,8 +13,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/owners")
 public class OwnerController {
 
+    private final OwnerMapService ownerMapService;
+
+    public OwnerController(OwnerMapService ownerMapService) {
+        this.ownerMapService = ownerMapService;
+    }
+
     @RequestMapping({"/", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+
+        model.addAttribute("owners", this.ownerMapService.findAll());
+
         return "owners/index";
     }
 
